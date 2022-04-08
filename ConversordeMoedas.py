657e5ce4fa253f1fd7b66ffa63d3ad9176c30d00
Renato Barbosa <98436200+ORenatoBarbosa@users.noreply.github.com>
2022-04-07 21:22:52 -0300

    Create ConversordeMoedas.py

---
+++ ConversordeMoedas.py
@@ -0,0 +1,42 @@
+money = float(input('Quanto dinheiro vc tem na carteira? '))
+
+#1 dolar == 4.75 reais
+
+conversao_dolar = money / 4.75 #convertendo para dolar
+conversao_euro = money / 5.17 #convetrendo para euro
+conversao_iene = money / 0.037 #convertendo para iene
+conversao_pesoargentino = money / 0.042 #convertendo para peso argentino
+conversao_pesouruguaio = money / 0.11 #convertendo para peso Uruguaio
+conversao_guaraniparaguaio = money / 0.00069 #convertendo para Guarani Paraguaio
+conversao_bolivianodabolivia = money / 0.69 #convertendo para Boliviano da Bolivia
+conversao_dolarsingapuriano = money / 3.49 #convertendo para dolarsingapuriano
+conversao_pesomexicano = money / 0.24 #convertendo para pesomexicano
+conversao_dinarkuwaitiano = money / 15.60 #convertendo para dinarkuwaitiano
+
+
+
+
+print('Com {} reais voce consegue comprar {} dolares'.format(money,conversao_dolar))
+print('\n')
+print('Com {} reais voce pode comprar {} euros'.format(money,conversao_euro))
+print('\n')
+print('Com {} reais voce pode comprar {} ienes'.format(money,conversao_iene))
+print('\n')
+print('Com {} reais voce pode comprar {} pesosargentino'.format(money,conversao_pesoargentino))
+print('\n')
+print('Com {} reais voce pode comprar {} pesosuruguaio'.format(money,conversao_pesouruguaio))
+print('\n')
+print('Com {} reais voce pode comprar {} guaranisparaguaio'.format(money,conversao_guaraniparaguaio))
+print('\n')
+print('Com {} reais voce pode comprar {} bolivianosdabolivia'.format(money,conversao_bolivianodabolivia))
+print('\n')
+print('Com {} reais voce pode comprar {} dolares singapuriano'.format(money,conversao_dolarsingapuriano))
+print('\n')
+print('Com {} reais voce pode comprar {} pesosmexicanos'.format(money,conversao_pesomexicano))
+print('\n')
+print('Com {} reais voce pode comprar {} dinarkuwaitiano'.format(money,conversao_dinarkuwaitiano))
+print('\n')
+
+# conversor de Moedas atualizado em 2022, as moedas estão em constantes mudanças conforme o mercado financeiro.
+
+# Renato Barbosa
